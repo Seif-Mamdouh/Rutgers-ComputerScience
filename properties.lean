99baab97c32f24b21a488f938ab732968d8f9878
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-02-09 15:43:03 -0500

    Propostional on Lean

---
+++ properties.lean
@@ -0,0 +1,26 @@
+-- Not 
+[Unicode: ¬      Ascii: not    Lean: \not, \neg]
+
+-- And 
+[Unicode: /\      Ascii: /\     Lean: ∧]
+
+-- or 
+[Unicode: \/      Ascii: or \/   Lean: \or]
+
+-- implication
+[Unicode:  →      Ascii: ->      Lean: \to, \r, \imp]
+
+-- bi-implication
+[Unicode: ↔   Ascii:  <->        Lean: \iff, \lr]
+
+-- forall
+[Unicode: ∀   Ascii: forall       Lean: \all]
+
+--exists
+[Unicode: ∃   Ascii: exists      Lean:\ex]
+
+--fun 
+[Unicode: λ   Ascii: fun         Lean: \lam, \fun]
+
+-- not equal
+[Unicode:≠    Ascii: ~=      Lean; \ne]
