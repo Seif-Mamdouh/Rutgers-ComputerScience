f4189c4c2797b2476c8c463f55f601363fc05603
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-09-19 13:26:45 -0400

    grind

--- PrincipalProgramming/ocamlPrac.ml
+++ PrincipalProgramming/ocamlPrac.ml
@@ -1,6 +1,6 @@
 (* floating points *)
 
-let average_float a b = 
+(* let average_float a b = 
   (a +. b) /. 2.0 ;; 
 
   average_float 2.0 3.4;;
@@ -13,20 +13,20 @@ let average_float a b =
 let average_float3 a b c d = 
   (a *. b *. c*. d) /. 3.2;;
 
-  average_float3 2.4 5.6 1.3 8.9;; 
+  average_float3 2.4 5.6 1.3 8.9;;  *)
 
 
   (* Recursive Functions *)
   (* Recrusvive is the function that calls itself within itslef *)
   (* in Ocaml we have we use 'rec' to show ocaml that we are using recursion alog *)
 
-  let rec range a b = 
+  (* let rec range a b = 
     if a > b then []
     else a :: range (a + 1 ) b;;
 
     let digits = range 0 20;;
 
-    digits;;
+    digits;; *)
 
     (* This is a expression not a statement 
 
@@ -37,3 +37,63 @@ let average_float3 a b c d =
        A statement can be simple or complex and can contain 0 or more expression.
 
        *)
+
+(* let rec factorial n =
+    match n with
+    | 0 | 1 -> 1
+    | x -> x * factorial (x - 1);; *)
+
+
+
+
+    (* Pattern Matching *)
+
+    (* Instead of using if then else 
+       We an use 'match' keyword to match on multiple possible values 
+    *)
+
+let rec factorial n = 
+  match n with 
+  | 0 | 1 -> 1 
+  | x -> x * factorial (x - 1);;
+
+  factorial 0
+
+(* instead of using x in the second line and use the '_' and use w 
+    _ which matches anything 
+ *)
+
+let rec factorial2 w = 
+  match w with 
+  | 0 | 1 -> 1
+  | _ -> w * factorial (w - 1);;
+
+factorial 3
+
+let factorial3 q = function
+  | 0 | 1 -> 1
+  | n -> n * factorial (q - 1);;
+
+  factorial3 4
+
+
+(* Lists *)
+
+(* Lists are a common compound data type in OCaml. They are ordered collections of elements of like type: *)
+
+(* [];;
+- : 'a list = []
+# [1; 2; 3];;
+- : int list = [1; 2; 3]
+# [false; false; true];;
+- : bool list = [false; false; true]
+# [[1; 2]; [3; 4]; [5; 6]];;
+- : int list list = [[1; 2]; [3; 4]; [5; 6]] *)
+
+(* The head is the first element in the Linked List and the 
+   tail is the rest of the elements of the Linked List *)
+
+   1 :: [2; 3];;
+   [3] @ [4; 5];;
+
+   
