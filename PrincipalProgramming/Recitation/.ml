a1b2aba536b534eaddcbd1233175509b70b19704
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-09-21 18:25:07 -0400

    damn bro I can teach better than him

--- PrincipalProgramming/Recitation/.ml
+++ PrincipalProgramming/Recitation/.ml
@@ -1,4 +1,27 @@
-let rec append a b =
-    match a with
-    | [] -> b
-    | h :: t -> h :: append t b;;
+(* Functions Syntax *)
+(* let <function name> <inputs> = <outputs> *)
+
+let add x = x + 2;;
+
+add 2;;
+
+let addFloat y = y +. 3.2;;
+
+addFloat 3.2;;
+
+
+let multiply j h = j * h ;;
+
+multiply 2 3;;
+
+
+let int_to_month i = 
+  match i with 
+  | 1 -> "Jan"
+  | 2 -> "Feb"
+  | 3 -> "March"
+  | 4 -> "April"
+  | _ -> "None";;
+
+
+int_to_month 0;;
