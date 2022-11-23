14c3361d607d901a46e580d15298f75e5f5d9232
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2022-11-23 11:07:25 -0500

    prolog is myh bitch

--- PrincipalProgramming/Assigements/assignment4/assignment4.pl
+++ PrincipalProgramming/Assigements/assignment4/assignment4.pl
@@ -6,7 +6,6 @@ reverseL([H|T],L):-
     reverseL(T,R),
     append(R,[H],L).
 
-
 ?- reverseL([],X).
 ?- reverseL([1,2,3],X).
 ?- reverseL([a,b,c],X).
@@ -37,6 +36,7 @@ assoc_list(L,AL) :- false.
 ?- assoc_list([1,1,4,2,2,2,3,1,1,3,1], X).
 
 /* YOUR CODE HERE (Problem 4, delete the following line) */
+
 % intersectionL(L1,L2,L3) :- false.
 intersection([], _, []):- !. %if first list is empty
 intersection(_, [], []):- !. %if second list is empty
@@ -67,14 +67,36 @@ partition(L,P,S) :- false.
 ?- partition([a,b,c,d],X,Y).
 
 /* YOUR CODE HERE (Problem 7, delete the following line) */
-merge(X,Y,Z) :- false.
+% merge(X,Y,Z) :- false.
+merge([], [], []):- !. %if both lists are empty
+merge(L1, [], L1):- !. %if second list is empty
+merge([], L2, L2):- !. %if first list is empty
+%if first element of first list is less than first element of second list
+merge([X|L1], [Y|L2], Z):- X<Y, merge(L1, [Y|L2], Z1), append([X], Z1, Z), !.
+%else if first element of second list is less than first element of first list
+merge(L1, [Y|L2], Z):- merge(L1, L2, Z1), append([Y], Z1, Z).
+
 
 ?- merge([],[1],[1]).
 ?- merge([1],[],[1]).
 ?- merge([1,3,5],[2,4,6],X).
 
 /* YOUR CODE HERE (Problem 8, delete the following line) */
-mergesort(L,SL) :- false.
+% mergesort(L,SL) :- false.
+
+split_in_half(Xs, Ys, Zs):-
+    length(Xs, Len), 
+    Half is Len // 2, % // denotes integer division, rounding down 
+    split_at(Xs, Half, Ys, Zs).
+%split_at(Xs, Half, Ys, Zs) divides Xs into a list Ys of length N
+% and a list of Ys containing the part after the first N.
+split_at(Xs, N, Ys, Zs):-
+    length(Ys, N),
+    apend(Ys, Zs, Xs).
+mergesort(Xs, S):- 
+    length(Xs, Len),
+    (Len =< 2 -> S = Xs; split_in_half(Xs, Ys, Zs), mergesort(Ys, SY), mergesort(Zs, SZ), merge(SY, SZ, S)).
+
 
 ?- mergesort([3,2,1],X).
 ?- mergesort([1,2,3],Y).
