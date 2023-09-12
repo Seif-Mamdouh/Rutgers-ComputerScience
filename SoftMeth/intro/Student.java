5c11d5b0cded43fa25d39a613d72c3bbdc8f77b1
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2023-09-12 17:40:05 -0400

    refrecne types

---
+++ SoftMeth/intro/Student.java
@@ -0,0 +1,17 @@
+package intro;
+
+// refrence types that that has default valuees
+
+public public class Student {
+
+    private String name;
+    private String last_name;
+    private int grade;
+
+
+    public static void main(String[] args){
+        Student student = new Student ();
+        System.out.println(student.name);
+        System.out.println(student.grade);
+    }
+}
